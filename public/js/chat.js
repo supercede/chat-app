c2d8fad9da3c5374e617a8a316156999a3248ffd
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-02-09 15:23:34 +0100

    enable event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,9 @@
 const increase = document.getElementById('increment');
 const msgForm = document.getElementById('message-form');
 const locationBtn = document.getElementById('send-location');
+const msgInput = document.getElementById('message');
+const sendBtn = document.querySelector('.send-btn');
+
 const socket = io();
 
 socket.on('message', message => {
@@ -9,20 +12,38 @@ socket.on('message', message => {
 
 msgForm.addEventListener('submit', e => {
   e.preventDefault();
-  const userMessage = document.getElementById('message').value;
 
-  socket.emit('sendMessage', userMessage);
-  document.getElementById('message').value = '';
+  sendBtn.setAttribute('disabled', 'disabled');
+
+  const userMessage = msgInput.value;
+
+  socket.emit('sendMessage', userMessage, err => {
+    sendBtn.removeAttribute('disabled');
+    msgInput.value = '';
+    msgInput.focus();
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`Message delivered`);
+  });
 });
 
 locationBtn.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Your browser does not support geolocation');
   }
+  locationBtn.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition(position => {
-    socket.emit('sendLocation', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      () => {
+        locationBtn.removeAttribute('disabled');
+        console.log('You shared your location');
+      }
+    );
   });
 });
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import http from 'http';
 import path from 'path';
 import logger from 'morgan';
 import socketio from 'socket.io';
+import Filter from 'bad-words';
 
 config();
 
@@ -19,19 +20,29 @@ if (NODE_ENV === 'development') {
 const staticPath = path.join(__dirname, '../public');
 app.use(express.static(staticPath));
 
+const filter = new Filter();
+filter.removeWords('hells', 'sadist', 'nigga');
 const message = 'Welcome';
+
 io.on('connection', socket => {
   console.log('New Connection');
 
   socket.emit('message', message);
   socket.broadcast.emit('message', 'A new user has joined');
 
-  socket.on('sendMessage', userMessage => {
+  //filter words with acknowlegement
+  socket.on('sendMessage', (userMessage, callback) => {
+    // if (filter.isProfane(userMessage)) {
+    //   return callback('profanity not allowed');
+    // }
+    userMessage = filter.clean(userMessage);
     io.emit('message', userMessage);
+    callback();
   });
 
-  socket.on('sendLocation', ({ latitude, longitude }) => {
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
     io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+    callback();
   });
 
   socket.on('disconnect', () => {
